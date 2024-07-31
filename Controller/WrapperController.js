f42b0819649c7b91182539c5120907100361902c
SolarisQuests <160103412+SolarisQuests@users.noreply.github.com>
2024-07-31 17:00:52 +0530

    done

--- Controller/WrapperController.js
+++ Controller/WrapperController.js
@@ -15,100 +15,6 @@ const db = client.db(dbName);
 
 
 
-// export const FetchCalls = async (req, res) => {
-//   try {
- 
-//     const accountSid = process.env.TWILIO_ACCOUNT_SID;
-
-//     const authToken = process.env.TWILIO_AUTH_TOKEN;
-//     const client = twilio(accountSid, authToken);
-//     console.log('Fetching and storing recordings...');
-//     const targetNumbers = [
-//       '+13462751361',
-//       '+13462755401'
-//      ];
-   
-// //     const threeDaysAgo = new Date();
-// // threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
-// // console.log(threeDaysAgo)
-// const calls = await client.calls.list({});
-
- 
-//   const filteredCalls = calls.filter(call => {
-//     return targetNumbers.includes(call.to) || targetNumbers.includes(call.from);
-//   });
-//   console.log(filteredCalls)
-//     // const callToMap = new Map();
-//     // const callFroMMap = new Map();
-//     // const oldcollection = db.collection('recordings_rows_two');
-//     // for (let call of filteredCalls ) {
-//     //   // console.log( call.to,call.from)
-//     //   callToMap.set(call.sid, call.to);
-//     //   callFroMMap.set(call.sid, call.from)
-//     //   const existingRecording = await oldcollection.findOne({ call_sid: call.sid });
-//     //   if (!existingRecording) {
-//     //     // console.log(`Call SID: ${call.sid}, From: ${call.from}, To: ${call.to}, Status: ${call.status},${call}`);
-
-//     //     const callRecordings = await client.recordings.list({ callSid: call.sid });
-//     //     const to = callToMap.get(call.sid);
-//     //     const from = callFroMMap.get(call.sid);
-//     //     if (to) {
-//     //       callRecordings.forEach(recording => {
-//     //         recording.to = to;
-
-//     //       });
-//     //     }
-//     //     if (from) {
-//     //       callRecordings.forEach(recording => {
-
-//     //         recording.from = from;
-//     //       });
-//     //     }
-//     //     console.log('Call Recordings:', callRecordings);
-
-//     //     if (callRecordings.length > 0) {
-//     //       const serializedRecordings = callRecordings.map(recording => {
-//     //         return JSON.stringify({
-//     //           _id: recording.sid,
-//     //           _airbyte_unique_key: recording.sid,
-//     //           subresource_uris: recording.subresourceUris,
-//     //           date_updated: recording.dateUpdated,
-//     //           date_created: recording.dateCreated,
-//     //           source: recording.source,
-//     //           api_version: recording.apiVersion,
-//     //           uri: recording.uri,
-//     //           media_url: recording.mediaUrl,
-//     //           sid: recording.sid,
-//     //           duration: recording.duration,
-//     //           price_unit: recording.priceUnit,
-//     //           start_time: recording.startTime,
-//     //           channels: recording.channels,
-//     //           price: recording.price,
-//     //           call_sid: recording.callSid,
-//     //           account_sid: recording.accountSid,
-//     //           call_status: recording.status,
-//     //           to: recording.to,
-//     //           from: recording.from,
-//     //           _airbyte_ab_id: recording._airbyte_ab_id,
-//     //           _airbyte_emitted_at: recording._airbyte_emitted_at,
-//     //           _airbyte_normalized_at: recording._airbyte_normalized_at,
-//     //           _airbyte_recordings_hashid: recording._airbyte_recordings_hashid
-//     //         });
-//     //       });
-
-//     //       const result = await oldcollection.insertMany(serializedRecordings.map(serialized => JSON.parse(serialized)));
-//     //       // console.log(`${result.insertedCount} recordings inserted into MongoDB`);
-//     //     } else {
-//     //       console.log('No recordings found for this call.');
-//     //     }
-//     //   }
-//     // }
-
-//     console.log('Recording fetching and storing complete.');
-//   } catch (error) {
-//     console.error('Error fetching and storing recordings:', error);
-//   }
-// }
 
 
 export const FetchCalls = async (req, res) => {
@@ -164,69 +70,103 @@ export const FetchCalls = async (req, res) => {
     const callToMap = new Map();
     const callFroMMap = new Map();
     const oldcollection = db.collection('recordings_rows');
-    for (let call of filteredCalls ) {
-      // console.log( call.to,call.from)
-      callToMap.set(call.sid, call.to);
-      callFroMMap.set(call.sid, call.from)
-      const existingRecording = await oldcollection.findOne({ call_sid: call.sid });
-      if (!existingRecording) {
-        // console.log(`Call SID: ${call.sid}, From: ${call.from}, To: ${call.to}, Status: ${call.status},${call}`);
+    // for (let call of filteredCalls ) {
+    //   // console.log( call.to,call.from)
+    //   callToMap.set(call.sid, call.to);
+    //   callFroMMap.set(call.sid, call.from)
+    //   const existingRecording = await oldcollection.findOne({ call_sid: call.sid });
+    //   if (!existingRecording) {
+    //     // console.log(`Call SID: ${call.sid}, From: ${call.from}, To: ${call.to}, Status: ${call.status},${call}`);
 
-        const callRecordings = await client.recordings.list({ callSid: call.sid });
-        const to = callToMap.get(call.sid);
-        const from = callFroMMap.get(call.sid);
-        if (to) {
-          callRecordings.forEach(recording => {
-            recording.to = to;
+    //     const callRecordings = await client.recordings.list({ callSid: call.sid });
+    //     const to = callToMap.get(call.sid);
+    //     const from = callFroMMap.get(call.sid);
+    //     if (to) {
+    //       callRecordings.forEach(recording => {
+    //         recording.to = to;
 
-          });
-        }
-        if (from) {
-          callRecordings.forEach(recording => {
+    //       });
+    //     }
+    //     if (from) {
+    //       callRecordings.forEach(recording => {
 
-            recording.from = from;
-          });
-        }
-        console.log('Call Recordings:', callRecordings);
+    //         recording.from = from;
+    //       });
+    //     }
+    //     console.log('Call Recordings:', callRecordings);
 
-        if (callRecordings.length > 0) {
-          const serializedRecordings = callRecordings.map(recording => {
-            return JSON.stringify({
-              _id: recording.sid,
-              _airbyte_unique_key: recording.sid,
-              subresource_uris: recording.subresourceUris,
-              date_updated: recording.dateUpdated,
-              date_created: recording.dateCreated,
-              source: recording.source,
-              api_version: recording.apiVersion,
-              uri: recording.uri,
-              media_url: recording.mediaUrl,
-              sid: recording.sid,
-              duration: recording.duration,
-              price_unit: recording.priceUnit,
-              start_time: recording.startTime,
-              channels: recording.channels,
-              price: recording.price,
-              call_sid: recording.callSid,
-              account_sid: recording.accountSid,
-              call_status: recording.status,
-              to: recording.to,
-              from: recording.from,
-              _airbyte_ab_id: recording._airbyte_ab_id,
-              _airbyte_emitted_at: recording._airbyte_emitted_at,
-              _airbyte_normalized_at: recording._airbyte_normalized_at,
-              _airbyte_recordings_hashid: recording._airbyte_recordings_hashid
-            });
-          });
+    //     if (callRecordings.length > 0) {
+    //       const serializedRecordings = callRecordings.map(recording => {
+    //         return JSON.stringify({
+    //           _id: recording.sid,
+    //           _airbyte_unique_key: recording.sid,
+    //           subresource_uris: recording.subresourceUris,
+    //           date_updated: recording.dateUpdated,
+    //           date_created: recording.dateCreated,
+    //           source: recording.source,
+    //           api_version: recording.apiVersion,
+    //           uri: recording.uri,
+    //           media_url: recording.mediaUrl,
+    //           sid: recording.sid,
+    //           duration: recording.duration,
+    //           price_unit: recording.priceUnit,
+    //           start_time: recording.startTime,
+    //           channels: recording.channels,
+    //           price: recording.price,
+    //           call_sid: recording.callSid,
+    //           account_sid: recording.accountSid,
+    //           call_status: recording.status,
+    //           to: recording.to,
+    //           from: recording.from,
+    //           _airbyte_ab_id: recording._airbyte_ab_id,
+    //           _airbyte_emitted_at: recording._airbyte_emitted_at,
+    //           _airbyte_normalized_at: recording._airbyte_normalized_at,
+    //           _airbyte_recordings_hashid: recording._airbyte_recordings_hashid
+    //         });
+    //       });
 
-          const result = await oldcollection.insertMany(serializedRecordings.map(serialized => JSON.parse(serialized)));
-          // console.log(`${result.insertedCount} recordings inserted into MongoDB`);
-        } else {
-          console.log('No recordings found for this call.');
-        }
+    //       const result = await oldcollection.insertMany(serializedRecordings.map(serialized => JSON.parse(serialized)));
+    //       // console.log(`${result.insertedCount} recordings inserted into MongoDB`);
+    //     } else {
+    //       console.log('No recordings found for this call.');
+    //     }
+    //   }
+    // }
+
+    for (let call of filteredCalls) {
+      // Check if the call exists in the database, if not, insert it
+      const existingCall = await oldcollection.findOne({ call_sid: call.sid });
+      if (!existingCall) {
+        const callData = {
+          _id: call.sid,
+          call_sid: call.sid,
+          account_sid: call.accountSid,
+          from: call.from,
+          to: call.to,
+          status: call.status,
+          start_time: call.startTime,
+          end_time: call.endTime,
+          duration: call.duration,
+          date_created: call.dateCreated,
+          date_updated: call.dateUpdated,
+          direction: call.direction,
+          api_version: call.apiVersion,
+          price: call.price,
+          price_unit: call.priceUnit,
+          annotation: call.annotation,
+          forwarded_from: call.forwardedFrom,
+          group_sid: call.groupSid,
+          caller_name: call.callerName,
+          uri: call.uri
+        };
+
+        await oldcollection.updateOne(
+          { _id: call.sid },
+          { $set: callData },
+          { upsert: true }
+        );
       }
     }
-
     console.log('Recording fetching and storing complete.');
 
     // res.json(allCalls);
